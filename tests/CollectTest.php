09554eff1918c98759e8c3eeb86a60eacd8a5755
Saveliy Ivanov <savel.andr.ivanov@gmail.com>
2025-02-15 01:53:29 +0700

    mycommit

--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -10,4 +10,24 @@ class CollectTest extends TestCase
         $this->assertSame(2, $collect->count());
     }
 
+    public function testPush()
+    {
+        $collect = new \Collect\Collect(['a' => 13]);
+        $collect->push(17, 'b');
+        $this->assertSame(['a' => 13, 'b' => 17], $collect->toArray());
+    }
+
+    public function testUnshift()
+    {
+        $collect = new \Collect\Collect(['a' => 13]);
+        $collect->unshift(10);
+        $this->assertSame([0 => 10, 'a' => 13], $collect->toArray());
+    }
+
+    public function testShift()
+    {
+        $collect = new \Collect\Collect(['a' => 13, 'b' => 17]);
+        $collect->shift();
+        $this->assertSame(['b' => 17], $collect->toArray());
+    }
 }
